d642009b6b69f57a53b91e7b7d22a988d7abbb83
selcuksert <selcuk.sert@gmail.com>
2021-08-28 11:30:04 +0300

    Moved to compose

--- docker/compose/down.sh
+++ docker/compose/down.sh
@@ -1,8 +1,15 @@
 PSScriptRoot=$(dirname "$0")
+NETWORK_NAME="netipvlan"
 
 docker-compose -f "$PSScriptRoot"/docker-compose.yml down
 
 if [ -d "$PSScriptRoot"/logs ]
 then
   rm -rf "$PSScriptRoot"/logs
+fi
+
+if docker network inspect $NETWORK_NAME &>/dev/null
+then
+  echo "Removing network"
+  docker network rm $NETWORK_NAME
 fi
--- docker/compose/up.sh
+++ docker/compose/up.sh
@@ -1,5 +1,15 @@
 #!/bin/zsh
 
 PSScriptRoot=$(dirname "$0")
+NETWORK_NAME="netipvlan"
 
+if ! docker network inspect $NETWORK_NAME &>/dev/null
+then
+  echo "Creating network"
+  docker network create -d ipvlan -o parent=eth0 \
+  -o ipvlan_mode=l2 \
+  --subnet=192.168.1.0/24 \
+  --gateway=192.168.1.1 \
+  --ip-range=192.168.1.192/27 $NETWORK_NAME
+fi
 docker-compose -f "$PSScriptRoot"/docker-compose.yml up -d --scale receiver=3 --scale sender=1
